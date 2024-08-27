fa9c5156944479152fd142bf1de1d83ee477ac51
Kyle Rockman <kyle@opslevel.com>
2024-08-27 12:04:56 -0500

    ensure module works

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -5,3 +5,20 @@ module "platform_team" {
   source = "../../modules/team"
   name   = "Platform"
 }
+
+module "filter" {
+  source = "../../modules/filter"
+  name   = "Platform Services"
+  predicates = [
+    {
+      key    = "name"
+      type   = "contains"
+      value  = "Platform"
+    },
+    {
+      key    = "tier_index"
+      type   = "greater_than_or_equal_to"
+      value  = "2"
+    }
+  ]
+}
