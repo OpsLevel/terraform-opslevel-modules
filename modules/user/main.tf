d4bf98fec7b031c8ea8a95998ef55b6d4217e6ec
David Bloss <david@opslevel.com>
2024-09-05 15:58:20 -0500

    add force_send_invite variable to user module

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -1,7 +1,8 @@
 data "opslevel_users" "all" {}
 
 resource "opslevel_user" "this" {
-  email              = var.email
+  email = var.email
+  # force_send_invite  = var.force_send_invite  # TODO: uncomment on next release
   name               = var.name
   role               = var.role
   skip_welcome_email = var.skip_welcome_email
