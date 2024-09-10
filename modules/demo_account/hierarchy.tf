e986837edb209a14d785c4b0c35e18e60e1529a9
Kyle Rockman <kyle@opslevel.com>
2024-09-10 15:47:27 -0500

    Add systems and domains

---
+++ modules/demo_account/hierarchy.tf
@@ -0,0 +1,53 @@
+module "product" {
+  source = "../hierarchy"
+  domains = [
+    {
+      name        = "Retail Website"
+      description = "The main website for Big River Books."
+      owner       = module.executive-leadership-team.this.id
+      systems = [
+        {
+          name        = "Order Pipeline"
+          description = "The system that manages the order workflow."
+          owner       = module.product-engineering-team.this.id
+          #           services = [
+          #             module.order-workflow.this.id,
+          #             module.order-fulfillment.this.id
+          #           ]
+        },
+        {
+          name        = "Shopping Cart"
+          description = "The system that manages the shopping cart."
+          owner       = module.product-engineering-team.this.id
+          #           services = [
+          #             module.shopping-cart.this.id
+          #           ]
+        }
+      ]
+    },
+    {
+      name        = "Internal Services"
+      description = "Internal services that power the Big River Books company."
+      owner       = module.inventory-team.this.id
+      systems = [
+        {
+          name        = "Procurement"
+          description = "The system that manages inventory procurement."
+          owner       = module.inventory-team.this.id
+          #           services = [
+          #             module.procurement-service.this.id
+          #           ]
+        },
+        {
+          name        = "Tooling"
+          description = "Internal tools that help Big River Books employees."
+          owner       = module.internal-tools-team.this.id
+          #           services = [
+          #             module.certificate-manager.this.id,
+          #             module.employee-directory.this.id
+          #           ]
+        }
+      ]
+    }
+  ]
+}
