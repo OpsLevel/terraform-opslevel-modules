8ac9c0cd9a70da1c8cb097ae8d80077527321db3
Kyle Rockman <kyle@opslevel.com>
2024-09-10 15:35:09 -0500

    fix linting

--- modules/demo_account/users.tf
+++ modules/demo_account/users.tf
@@ -1,17 +1,17 @@
 locals {
   people = {
-    "John Doe": "john@example.com"
-    "Ken Doe": "ken@example.com"
-    "Adam Doe": "admin@example.com"
-    "Ian Doe": "ian@example.com"
-    "Toms Doe": "toms@example.com"
-    "Muriel Doe": "muriel@example.com"
-    "Ty Doe": "ty@example.com"
-    "Andrew Doe": "andrew@example.com"
-    "Stephanie Doe": "stephanie@example.com"
-    "Emily Doe": "emily@example.com"
-    "Kyle Doe": "kyle@example.com"
-    "David Doe": "david@example.com"
+    "John Doe" : "john@example.com"
+    "Ken Doe" : "ken@example.com"
+    "Adam Doe" : "admin@example.com"
+    "Ian Doe" : "ian@example.com"
+    "Toms Doe" : "toms@example.com"
+    "Muriel Doe" : "muriel@example.com"
+    "Ty Doe" : "ty@example.com"
+    "Andrew Doe" : "andrew@example.com"
+    "Stephanie Doe" : "stephanie@example.com"
+    "Emily Doe" : "emily@example.com"
+    "Kyle Doe" : "kyle@example.com"
+    "David Doe" : "david@example.com"
   }
 }
 
