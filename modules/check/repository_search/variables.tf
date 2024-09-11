23f1b29304d837def5bd1c81d242a46b0fa896e1
David Bloss <david@opslevel.com>
2024-09-11 16:11:32 -0500

    fill in remaining check variables, etc

---
+++ modules/check/repository_search/variables.tf
@@ -0,0 +1,14 @@
+variable "file_extensions" {
+  type        = set(string)
+  description = "Restrict the search to files of given extensions. Extensions should contain only letters and numbers. For example: [\"py\", \"rb\"]."
+  default     = null
+}
+
+variable "file_contents_predicate" {
+  type = object({
+    type  = string
+    value = optional(string)
+  })
+  description = "A condition that should be satisfied."
+}
+
