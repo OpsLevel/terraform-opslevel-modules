2914a0c7075a9900b4fff0b02bb98d282680c963
Kyle Rockman <kyle@opslevel.com>
2024-09-11 09:33:00 -0500

    fix lint

--- modules/demo_account/sbom.tf
+++ modules/demo_account/sbom.tf
@@ -1,5 +1,5 @@
 locals {
-  sbom_good = templatefile("${path.module}/sboms/good.json", { })
-  sbom_good2 = templatefile("${path.module}/sboms/good2.json", { })
-  sbom_minimal = templatefile("${path.module}/sboms/minimal.json", { })
+  sbom_good    = templatefile("${path.module}/sboms/good.json", {})
+  sbom_good2   = templatefile("${path.module}/sboms/good2.json", {})
+  sbom_minimal = templatefile("${path.module}/sboms/minimal.json", {})
 }
