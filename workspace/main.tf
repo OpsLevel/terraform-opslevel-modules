95d94dabf6b07df71bb121a303d236c42b287812
Kyle Rockman <kyle@opslevel.com>
2024-09-12 13:59:43 -0500

    generate services

--- workspace/main.tf
+++ workspace/main.tf
@@ -4,5 +4,6 @@ provider "opslevel" {
 module "account" {
   source = "../modules/demo_account"
 
-  account_name = ""
+  account_name = "Rockman Enterprises"
+  #generate_services = 500  # Due note that due to API rate limits generating 500 services will take upwards of 10 mins and will significantly increase the time of future apply operations
 }
