4ee43b351b2bc62bbf8186435a4f0305a8e004ed
David Bloss <david@opslevel.com>
2024-09-12 11:09:05 -0500

    alphabetize it

--- modules/service/repository/main.tf
+++ modules/service/repository/main.tf
@@ -3,6 +3,6 @@ resource "opslevel_service_repository" "this" {
   name             = var.name
   repository       = var.repository
   repository_alias = var.repository_alias
-  service_alias    = var.service_alias
   service          = var.service
+  service_alias    = var.service_alias
 }
