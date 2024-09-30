a24909997c6f047532ec394a9ed2e88599d7aea6
David Bloss <david@opslevel.com>
2024-09-30 12:33:28 -0500

    add send_invite option to user module

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -2,5 +2,6 @@ resource "opslevel_user" "this" {
   email              = var.email
   name               = var.name
   role               = var.role
+  send_invite        = var.send_invite
   skip_welcome_email = var.skip_welcome_email
 }
