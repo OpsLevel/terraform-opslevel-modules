bad88f4ec772fac02c8426845292e9d356d301a7
David Bloss <david@opslevel.com>
2024-10-18 15:17:14 -0500

    walk back unneeded update

--- modules/check/base_variables.tf
+++ modules/check/base_variables.tf
@@ -42,14 +42,3 @@ variable "owner" {
   description = "The id of the team that owns the check."
   default     = null
 }
-
-locals {
-  rubric_category = var.category == null ? null : startswith(var.category, "Z2lkOi8v") ? var.category : element([
-    for category in data.opslevel_rubric_categories.all.rubric_categories :
-    category.id if category.alias == var.category
-  ], 0)
-  rubric_level = var.level == null ? null : startswith(var.level, "Z2lkOi8v") ? var.level : element([
-    for level in data.opslevel_rubric_levels.all.rubric_levels :
-    level.id if level.alias == var.level
-  ], 0)
-}
