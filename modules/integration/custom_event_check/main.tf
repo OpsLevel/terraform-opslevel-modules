8d2ea9d0bb5d419b2ef28ae52d203a2d552b2e23
David Bloss <david@opslevel.com>
2024-10-23 12:28:46 -0500

    add integration module for custom event check

---
+++ modules/integration/custom_event_check/main.tf
@@ -0,0 +1,4 @@
+resource "opslevel_integration_custom_event_check" "this" {
+  name = var.name
+  type = var.type
+}
---
+++ modules/integration/custom_event_check/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "The name of the integration."
+}
+
+variable "type" {
+  type        = string
+  description = "The type of the custom event check integration."
+}
