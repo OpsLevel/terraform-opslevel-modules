3abaa7dc6e52bd4c40ccb20da5ff0a95b3ef6156
David Bloss <david@opslevel.com>
2024-12-11 11:43:21 -0600

    move service CSV into demo_account

--- modules/demo_account/services.tf
+++ modules/demo_account/services.tf
@@ -1,17 +1,52 @@
-module "from_csv" {
-  for_each = { for svc in var.services_from_csv : svc.service_name => svc }
+locals {
+  example_csv = [{
+    "Aliases"      = "cart_ninja, ninja, shopping_cart",
+    "Created"      = formatdate("MMM DD, YYYY", timestamp()),
+    "Description"  = "Shopping cart management service",
+    "Service Name" = "cart-ninja",
+    "Team Owner"   = "Midnight Bandits",
+    "Tier"         = "Tier 1",
+    "Tools"        = "GitHub, PagerDuty, Datadog",
+  }]
+  csv_path = "${path.module}/services.csv"
+  csv_data = fileexists(local.csv_path) ? csvdecode(file(local.csv_path)) : local.example_csv
+  csv_services = [
+    for service in local.csv_data : {
+      for key, value in service :
+      lower(replace(key, " ", "_")) => value
+    }
+  ]
+  # NOTE: starter example for service tools
+  datadog_svc_tool = {
+    category    = "metrics"
+    environment = "Production"
+    name        = "Datadog"
+    url         = "https://datadoghq.com"
+  }
+}
 
-  source          = "../service"
-  name            = each.value.service_name
-  aliases         = toset([each.value.aliases])
-  description     = each.value.description
-  framework       = each.value.framework
-  language        = each.value.language
-  lifecycle_alias = each.value.lifecycle_alias
-  owner           = each.value.team_owner
-  tier_alias      = each.value.tier
-  tags            = ["db:mysql", "k8s:true", "build_speed:fast"]
-  # tools           = each.value.tools
+module "services_from_csv" {
+  for_each = { for svc in local.csv_services : svc.service_name => svc }
+
+  source      = "../service"
+  name        = each.value.service_name
+  aliases     = toset(split(",", each.value.aliases))
+  description = each.value.description
+  owner       = each.value.team_owner
+  tier_alias  = each.value.tier
+  tags        = ["db:mysql", "k8s:true", "build_speed:fast"]
+  # NOTE: add to CSV file?
+  # framework       = each.value.framework
+  # language        = each.value.language
+  # lifecycle_alias = each.value.lifecycle_alias
+  tools = [
+    for tool_name in split(",", each.value.tools) : lower(tool_name) == "datadog" ? local.datadog_svc_tool : {
+      category    = "observability"
+      environment = "Production"
+      name        = tool_name
+      url         = "https://example.com/example"
+    }
+  ]
   repositories = local.repository
   properties   = local.properties
 }
--- workspace/main.tf
+++ workspace/main.tf
@@ -3,29 +3,8 @@ provider "opslevel" {}
 module "account" {
   source = "../modules/demo_account"
 
-  account_token     = var.account_token
-  account_name      = "Big River Books"
-  account_users     = [var.user_email]
-  services_from_csv = local.csv_services
+  account_token = var.account_token
+  account_name  = "Big River Books"
+  account_users = [var.user_email]
   #generate_services = 500  # Due note that due to API rate limits generating 500 services will take upwards of 10 mins and will significantly increase the time of future apply operations
-}
-
-locals {
-  example_csv = [{
-    "Aliases"      = "cart_ninja, ninja, shopping_cart",
-    "Created"      = formatdate("MMM DD, YYYY", timestamp()),
-    "Description"  = "Shopping cart management service",
-    "Service Name" = "cart-ninja",
-    "Team Owner"   = "Midnight Bandits",
-    "Tier"         = "Tier 1",
-    "Tools"        = "GitHub, PagerDuty, Datadog",
-  }]
-  csv_path = "${path.module}/services.csv"
-  csv_data = fileexists(local.csv_path) ? csvdecode(file(local.csv_path)) : local.example_csv
-  csv_services = [
-    for service in local.csv_data : {
-      for key, value in service :
-      lower(replace(key, " ", "_")) => value
-    }
-  ]
 }
