57f41db024e97ac1fed5acb609fffae4819db002
David Bloss <david@opslevel.com>
2024-12-11 11:48:07 -0600

    clean up

--- modules/demo_account/services_generated.tf
+++ modules/demo_account/services_generated.tf
@@ -1,18 +1,3 @@
-# variable "generate_services" {
-#   description = "The number of services to generate."
-#   type        = number
-#   default     = 0
-# }
-
-# resource "random_pet" "services" {
-#   count = var.generate_services
-
-#   keepers = {
-#     # Generate a new pet names each time we switch the count
-#     count = var.generate_services
-#   }
-# }
-
 module "ai-team" {
   source = "../team"
   name   = "AI Team"
@@ -24,13 +9,3 @@ module "ai-team" {
     },
   ]
 }
-
-# # We had to use the resource directly here because the module does a query for all services per service generated which is SUPER heavy.
-# module "services_generated" {
-#   source = "../service"
-#   count  = var.generate_services
-
-#   name  = random_pet.services[count.index].id
-#   owner = module.ai-team.this.id
-# }
-
