0ae4356c2f1bad12a9fc7c915e2b39ab54f80217
David Bloss <david@opslevel.com>
2024-12-13 11:19:39 -0600

    add columns to services.csv, apply to services in demo_account

--- modules/demo_account/services.tf
+++ modules/demo_account/services.tf
@@ -6,36 +6,28 @@ locals {
       lower(replace(key, " ", "_")) => value
     }
   ]
-  # NOTE: starter example for service tools
-  datadog_svc_tool = {
-    category    = "metrics"
-    environment = "Production"
-    name        = "Datadog"
-    url         = "https://datadoghq.com"
-  }
 }
 
 module "services_from_csv" {
   for_each = { for svc in local.csv_services : svc.service_name => svc }
 
-  source      = "../service"
-  name        = each.value.service_name
-  aliases     = toset(split(",", each.value.aliases))
-  description = each.value.description
-  owner       = each.value.team_owner
-  tier_alias  = each.value.tier
-  tags        = ["db:mysql", "k8s:true", "build_speed:fast"]
-  # NOTE: add to CSV file?
-  # framework       = each.value.framework
-  # language        = each.value.language
-  # lifecycle_alias = each.value.lifecycle_alias
+  source          = "../service"
+  name            = each.value.service_name
+  aliases         = toset(split(",", each.value.aliases))
+  description     = each.value.description
+  framework       = each.value.framework
+  language        = each.value.language
+  lifecycle_alias = each.value.lifecycle
+  owner           = each.value.team_owner
+  tier_alias      = each.value.tier
+  tags            = ["db:mysql", "k8s:true", "build_speed:fast"]
   tools = [
-    for tool_name in split(",", each.value.tools) : lower(tool_name) == "datadog" ? local.datadog_svc_tool : {
-      category    = "observability"
-      environment = "Production"
-      name        = tool_name
+    for index, tool_name in split(",", each.value.tools) : lookup(local.tools, tool_name, {
+      name        = "Example-${index}",
+      category    = "runbooks",
+      environment = "Staging",
       url         = "https://example.com/example"
-    }
+    })
   ]
   repositories = local.repository
   properties   = local.properties
